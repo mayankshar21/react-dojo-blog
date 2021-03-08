e02efb2875c7af1b4712158b7e6beb44dab68fd8
Mayank Sharma <mayanks1@student.unimelb.edu.au>
2021-03-08 20:48:20 +0800

    Created react application

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// kick starts our application
+// takes all our react components and mounting them to the dom
+// React strict - does additional checks and shows warnings if there are any warnings to report
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -14,4 +15,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
